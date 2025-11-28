53988c8865246c736b67a2e7a6fe377b6a5d7811
XIARuijun <1814268532@qq.com>
2025-11-28 11:39:21 +0800

    U

--- LLM/CUHK-X-VLM/src/action_selection/exp.sh
+++ LLM/CUHK-X-VLM/src/action_selection/exp.sh
@@ -1,11 +1,11 @@
-#CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/action_selection/main_internvl_choices.py --modality depth --model_size 3B
+#CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/action_selection/main_internvl_choices.py --modality depth --model_size 2B
 #CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/action_selection/main_internvl_choices --modality depth --model_size 8B
 #CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/action_selection/main_qwenvl_choices --modality depth --model_size 3B
 #CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/action_selection/main_qwenvl_choices --modality depth --model_size 7B
 #CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/action_selection/main_videochatr1_choices.py --modality depth
 #CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/action_selection/main_videollava_choices --modality depth
 
-#CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/action_selection/main_internvl_choices.py --modality rgb --model_size 3B
+#CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/action_selection/main_internvl_choices.py --modality rgb --model_size 2B
 #CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/action_selection/main_internvl_choices --modality rgb --model_size 8B
 #CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/action_selection/main_qwenvl_choices --modality rgb --model_size 3B
 #CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/action_selection/main_qwenvl_choices --modality rgb --model_size 7B
--- LLM/CUHK-X-VLM/src/task_caption/exp.sh
+++ LLM/CUHK-X-VLM/src/task_caption/exp.sh
@@ -1,11 +1,11 @@
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/task_caption/main_internvl.py --modality depth --model_size 3B
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/task_caption/main_internvl.py --modality depth --model_size 2B
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/task_caption/main_internvl.py --modality depth --model_size 8B
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/task_caption/main_qwenvl.py --modality depth --model_size 3B
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/task_caption/main_qwenvl.py --modality depth --model_size 7B
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/task_caption/main_videochatr1.py --modality depth
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/task_caption/main_videollava.py --modality depth
 
-# CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/task_caption/main_internvl.py --modality rgb --model_size 3B
+# CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/task_caption/main_internvl.py --modality rgb --model_size 2B
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/task_caption/main_internvl.py --modality rgb --model_size 8B
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/task_caption/main_qwenvl.py --modality rgb --model_size 7B
 # CUDA_VISIBLE_DEVICES=0,1,2,3 python CUHK-X-VLM/src/task_caption/main_qwenvl.py --modality rgb --model_size 3B
